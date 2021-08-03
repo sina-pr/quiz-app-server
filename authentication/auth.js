b9e52db59b3d163908d3a3f4264e777de283666c
Sina Sedighi <dev.sina2000@gmail.com>
2021-08-03 14:42:43 +0430

    update auth.js

--- authentication/auth.js
+++ authentication/auth.js
@@ -6,18 +6,3 @@ const secretKey = process.env.SECRET_KEY;
 exports.generateToken = (user) => {
   return jwt.sign({ userId: user._id, email: user.email }, secretKey);
 };
-/*
-exports.verifyToken = (req, res) => {
-  try {
-    const token = req.headers.authorization.split(" ")[0];
-    payload = jwt.verify(token, secretKey);
-    res.send(payload);
-  } catch {
-    return res.status(401).json({
-      statusCode: 401,
-      status: false,
-      message: "Access Restricted! Please Login",
-    });
-  }
-};
-*/
